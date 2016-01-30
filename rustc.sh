3adbd0280edbe6b017543ee4126842f5519e9a62
Nemanja Boric <4burgos@gmail.com>
2016-01-31 00:16:15 +0100

    Compile and with fPIE to avoid addressing problems (passing f. address)

--- rustc.sh
+++ rustc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-~/rust/arm-unknown-linux-gnueabi/bin/rustc -Z no-landing-pads --crate-type staticlib --target=arm-unknown-linux-gnueabi --emit obj -C linker=arm-linux-gnueabi-gcc  -C lto -C opt-level=3 $1
+~/rust/arm-unknown-linux-gnueabi/bin/rustc -Z no-landing-pads --crate-type staticlib --target=arm-unknown-linux-gnueabi --emit obj -C linker=arm-linux-gnueabi-gcc  -C lto -C opt-level=3 $1 -C relocation-model=dynamic-no-pic
