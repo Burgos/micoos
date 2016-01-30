09662ad3d2f4b48096524ad8afefd3155f471339
Nemanja Boric <4burgos@gmail.com>
2016-01-31 00:21:13 +0100

    Fill the IRQ vector from rust code

--- src/arm1176.rs
+++ src/arm1176.rs
@@ -146,7 +146,7 @@ fn enable_interrupts(interrupt: InterruptSources) -> () {
 
 // sets the address of the interrupt handler
 #[inline]
-fn set_irq_handler(src: InterruptSources, handler:unsafe extern "C" fn() -> ()) -> () {
+fn set_irq_handler(src: InterruptSources, handler: fn() -> ()) -> () {
     let reg = PrimaryInterruptControllerMap::PICVectAddr0 as u32 + 4*src as u32;
     let register_address = reg as *mut u32;
 
@@ -190,10 +190,7 @@ pub fn timer_interrupt_routine() -> () {
 
 #[no_mangle]
 pub fn enable_timer_interrupt() -> () {
-    extern {
-        fn interrupt_routine() -> ();
-    }
-    //set_irq_handler(InterruptSources::Timers01, interrupt_routine);
+    set_irq_handler(InterruptSources::Timers01, timer_interrupt_routine);
     setup_timer0();
     enable_interrupts(InterruptSources::Timers01);
 
