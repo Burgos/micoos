e0af6075bae7a51ec62af1ae2f4f56a7a35f2acc
Nemanja Boric <4burgos@gmail.com>
2016-02-03 21:57:20 +0100

    Call scheduled task from TimerTask every x ms

--- src/timer_task.rs
+++ src/timer_task.rs
@@ -26,6 +26,18 @@ impl TimerTask
     pub fn tick(&mut self) -> () {
         self.elapsed_ticks = self.elapsed_ticks + 1;
 
+        self.ticks_to_ms = self.ticks_to_ms - 1;
+        if (self.ticks_to_ms == 0)
+        {
+            self.ticks_to_ms = self.ticks_per_ms;
+            self.until_next_call_ms = self.until_next_call_ms - 1;
+
+            if (self.until_next_call_ms == 0)
+            {
+                self.until_next_call_ms = self.call_frequency_ms;
+                call_scheduled_task();
+            }
+        }
     }
 
     pub const fn new(ticks_per_ms: i32, call_frequency_ms: i32) -> TimerTask {
@@ -38,3 +50,8 @@ impl TimerTask
         }
     }
 }
+
+pub fn call_scheduled_task() -> () {
+     
+}
+
