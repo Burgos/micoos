c22c3c72516422c9ee5ce81563d9590f7bd0b0d0
Nemanja Boric <4burgos@gmail.com>
2016-02-03 21:57:59 +0100

    Fix outdated comments

--- src/vital.rs
+++ src/vital.rs
@@ -9,7 +9,6 @@ static mut timer: TimerTask = TimerTask::new(100, 20);
 // Timer interrupt, define and set
 #[no_mangle]
 pub fn timer_interrupt_routine() -> () {
-    // clear timer interrupt flag
     Register::new(0x101f1000 as *mut u32).set(60);
    
     // safe to do as it is called from the routine while
@@ -18,6 +17,4 @@ pub fn timer_interrupt_routine() -> () {
     unsafe {
         timer.tick();
     }
-    // clear the interrupt vector address register
-    //Register::new(PrimaryInterruptControllerMap::PICVectAddr as u32 as *mut u32).set(0);
 }
