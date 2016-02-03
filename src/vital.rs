e312e10fabf89f570939a87e26ef4359a6326e01
Nemanja Boric <4burgos@gmail.com>
2016-02-03 21:58:49 +0100

    Remove serial output from timer_interrupt_routine

--- src/vital.rs
+++ src/vital.rs
@@ -1,6 +1,5 @@
 // Core routines for keeping OS alive
 
-use register::Register;
 use timer_task::TimerTask;
 
 // lifetime timer task reference
@@ -9,8 +8,6 @@ static mut timer: TimerTask = TimerTask::new(100, 20);
 // Timer interrupt, define and set
 #[no_mangle]
 pub fn timer_interrupt_routine() -> () {
-    Register::new(0x101f1000 as *mut u32).set(60);
-   
     // safe to do as it is called from the routine while
     // no other timer interrupts might pop up as they are still
     // masked
