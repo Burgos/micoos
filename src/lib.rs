4a9ad7f1267ec44125a1cb597311e69211960592
Nemanja Boric <4burgos@gmail.com>
2016-02-03 22:18:08 +0100

    Remove loop from kernel()
    Kernel() should just initialize hardware and the system should
    drop into wfe loop to wait for the interrupt to happen.

--- src/lib.rs
+++ src/lib.rs
@@ -29,16 +29,5 @@ pub unsafe fn __aeabi_unwind_cpp_pr0() -> ()
 
 #[no_mangle]
 pub fn kernel() -> () {
-	let msg: &[u8] = unsafe { mem::transmute("MICO!\n") };
-	let serial_port = register::Register::new(0x101f1000 as *mut u8);
-
     arm1176::enable_timer_interrupt();
-
-    loop {
-        for i in 0..6 {
-            let current_time = arm1176::get_current_time();
-            let ascii_sec = current_time % 10 + 48;
-            serial_port.set(ascii_sec as u8);
-        }
-    }
 }
