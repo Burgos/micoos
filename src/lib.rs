cdcdbe57e68326204542e0c77137ca74b883ee7e
Nemanja Boric <4burgos@gmail.com>
2016-02-09 22:22:43 +0100

    Add __aeabi_memclr* methods

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,26 @@ pub unsafe fn __aeabi_unwind_cpp_pr0() -> ()
     loop {}
 }
 
+// http://stackoverflow.com/questions/31494087/linking-rust-with-c-undefined-reference-to-aeabi-functions
+#[no_mangle]
+pub unsafe extern fn __aeabi_memclr4(s: *mut u8, n: usize) -> *mut u8 {
+    let mut i = 0;
+    while i < n {
+        *s.offset(i as isize) = 0u8;
+        i += 1;
+    }
+    return s;
+} 
+
+#[no_mangle]
+pub unsafe extern fn __aeabi_memclr(s: *mut u8, n: usize) -> *mut u8 {
+    let mut i = 0;
+    while i < n {
+        *s.offset(i as isize) = 0u8;
+        i += 1;
+    }
+    return s;
+} 
 
 #[no_mangle]
 pub fn kernel() -> () {
