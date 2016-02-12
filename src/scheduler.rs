692ff841e05fc60b52ef363a03755d3cfcd2915c
Nemanja Boric <4burgos@gmail.com>
2016-02-12 23:00:27 +0100

    Implement add_process method in scheduler

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -3,6 +3,7 @@
 // context switch.
 
 use process::Process;
+use process::ProcessError;
 
 pub struct Scheduler {
     processes: [Process; 10],
@@ -14,16 +15,22 @@ pub struct Scheduler {
 impl Scheduler {
     pub fn new() -> Scheduler {
         Scheduler {
-            processes: [Process::new(); 10],
+            processes: [Process::new(dummy); 10],
             current_process: 0,
             number_of_processes: 0
         }
     }
 
-    pub fn add_process(&mut self, process: Process) {
-        self.processes[self.number_of_processes] = process;
+    pub fn add_process(&mut self, function_to_run: fn() -> ()) -> Result<(), ProcessError> {
+        try!(self.processes[self.number_of_processes].set_function_to_run(function_to_run));
+        try!(self.processes[self.number_of_processes].mark_process_ready());
+        try!(self.processes[self.number_of_processes].set_stack_pointer(self.number_of_processes));
         self.number_of_processes = self.number_of_processes + 1;
+        Ok(())
     }
+}
 
-
+// dummy process implementation
+fn dummy() -> () {
+    loop {}
 }
