152675c8a998c3337fdc3f19f8db6a957fa1d66f
Nemanja Boric <4burgos@gmail.com>
2016-02-12 23:21:45 +0100

    Add aeabi_memclr8

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,16 @@ pub unsafe extern fn __aeabi_memclr4(s: *mut u8, n: usize) -> *mut u8 {
 } 
 
 #[no_mangle]
+pub unsafe extern fn __aeabi_memclr8(s: *mut u8, n: usize) -> *mut u8 {
+    let mut i = 0;
+    while i < n {
+        *s.offset(i as isize) = 0u8;
+        i += 1;
+    }
+    return s;
+} 
+
+#[no_mangle]
 pub unsafe extern fn __aeabi_memclr(s: *mut u8, n: usize) -> *mut u8 {
     let mut i = 0;
     while i < n {
