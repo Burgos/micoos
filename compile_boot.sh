862f32611ba36806867f788f84878931482b4975
Nemanja Boric <4burgos@gmail.com>
2016-02-15 21:44:23 +0100

    Add umodsi3 and udivsi3 LLVM implementations
    http://opensource.apple.com/source/clang/clang-163.7.1/src/projects/compiler-rt/lib/arm/udivsi3.S
    http://opensource.apple.com/source/clang/clang-163.7.1/src/projects/compiler-rt/lib/arm/umodsi3.S
    http://opensource.apple.com/source/clang/clang-163.7.1/src/projects/compiler-rt/lib/assembly.h

--- compile_boot.sh
+++ compile_boot.sh
@@ -1,2 +1,4 @@
 arm-linux-gnueabi-gcc -fpie -ffreestanding -c boot.S -o boot.o -mcpu=arm1176jzf-s
 arm-linux-gnueabi-gcc -fpie -ffreestanding -c arm.c -o aeabi.a -mcpu=arm1176jzf-s -std=c99
+arm-linux-gnueabi-gcc -fpie -ffreestanding -c umodsi3.S -o umodsi3.o -mcpu=arm1176jzf-s
+arm-linux-gnueabi-gcc -fpie -ffreestanding -c udivsi3.S -o udivsi3.o -mcpu=arm1176jzf-s
--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-arm-linux-gnueabi-gcc  -Wl,--build-id=none -T arm.ld -nostartfiles -o kernel.elf -fpie -ffreestanding -nostdlib boot.o aeabi.a lib.o
+arm-linux-gnueabi-gcc  -Wl,--build-id=none -T arm.ld -nostartfiles -o kernel.elf -fpie -ffreestanding -nostdlib boot.o aeabi.a umodsi3.o udivsi3.o lib.o
 
