c6530f05a1f25ae709f2a6a1311e2715e02acdba
Nemanja Boric <4burgos@gmail.com>
2016-02-15 21:45:31 +0100

    Mark process ready only after setting the SP
    Because set_process_sp requires process that was just merely created,
    we need to mark process ready *after* sp was set for the process.

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -23,10 +23,11 @@ impl Scheduler {
 
     pub fn add_process(&mut self, function_to_run: fn() -> ()) -> Result<(), ProcessError> {
         try!(self.processes[self.number_of_processes].set_function_to_run(function_to_run));
-        try!(self.processes[self.number_of_processes].mark_process_ready());
         try!(self.processes[self.number_of_processes].set_stack_pointer(self.number_of_processes));
+        try!(self.processes[self.number_of_processes].mark_process_ready());
         self.number_of_processes = self.number_of_processes + 1;
         Ok(())
+
     }
 
     pub fn schedule_next(&mut self) -> ()
