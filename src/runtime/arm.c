b995c00ee734130c9a35d9a847783cb34966f710
Nemanja Boric <4burgos@gmail.com>
2016-02-29 23:14:19 +0100

    Add __aeabi_memcpy implementation

--- src/runtime/arm.c
+++ src/runtime/arm.c
@@ -40,3 +40,10 @@ void __aeabi_memcpy4(char* dest, char* src, unsigned int n)
     }
 }
 
+void __aeabi_memcpy(char* dest, char* src, unsigned int n) 
+{
+    for (unsigned int i = 0; i < n; i++)
+    {
+        *(dest + i) = *(src + i);
+    }
+}
