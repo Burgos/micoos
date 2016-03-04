5fa01980078fd6b03eaee9f69a51ca6e4db29b2e
Nemanja Boric <4burgos@gmail.com>
2016-03-04 20:21:43 +0100

    Add some comments to process.rs

--- src/process.rs
+++ src/process.rs
@@ -6,6 +6,9 @@ use msgbox::MessageBoxResult;
 use msgbox::Message;
 use vital::Vital;
 
+
+// Describes in which particular state
+// the process is
 #[derive(Copy, Clone, PartialEq)]
 pub enum ProcessState {
     CREATED,
@@ -16,11 +19,15 @@ pub enum ProcessState {
     READY,
 }
 
+// Describes the error when dealing the
+// process operation
 #[derive(PartialEq)]
 pub enum ProcessError {
     ProcessAlreadyRunning
 }
 
+
+// Process structure
 #[derive(Copy, Clone)]
 pub struct Process<'a> {
     quantum: i32,
@@ -32,12 +39,16 @@ pub struct Process<'a> {
     vital: *const Vital<'a>
 }
 
+// Describes the result of the tick method -
+// should the process yield or not.
 #[derive(PartialEq)]
 pub enum ProcessTickResult {
     Yield,
     DontYield
 }
 
+// Wrapper around the process keeeping it from
+// executing arbitrar code
 fn process_runner(process_body: fn() -> ()) {
         process_body();
 
@@ -46,6 +57,8 @@ fn process_runner(process_body: fn() -> ()) {
 }
 
 impl<'a> Process<'a> {
+
+    // Process constructor
     pub fn new(quantum: i32, process_body: fn() -> (), vital: *const Vital<'a>) -> Process<'a> {
         let mut p = Process {
             quantum: quantum,
@@ -67,20 +80,24 @@ impl<'a> Process<'a> {
         p
     }
 
+    // Saves the process context
     #[no_mangle]
     pub fn save_context(&mut self) -> () {
         // now, let's save all registers to PCB
         arm1176::save_context_to_stack(&mut self.registers);
     }
 
+    // Restores the process context
     pub fn restore_context(&mut self) -> () {
         arm1176::restore_context_from_stack(&mut self.registers);
     }
 
+    // Sends message to the process
     pub fn send_message(&mut self, msg: Message) -> Result<(), MessageBoxResult> {
         self.msgbox.send_message(msg)
     }
 
+    // Sets the runner function on process
     pub fn set_function_to_run (&mut self, function_to_run: fn() -> ()) -> Result<(), ProcessError> {
         if self.state != ProcessState::CREATED {
             return Err(ProcessError::ProcessAlreadyRunning);
@@ -90,6 +107,7 @@ impl<'a> Process<'a> {
         Ok(())
     }
 
+    // Marks the process ready to be executed
     pub fn mark_process_ready (&mut self) -> Result<(), ProcessError> {
         if self.state != ProcessState::CREATED {
             return Err(ProcessError::ProcessAlreadyRunning);
@@ -99,10 +117,12 @@ impl<'a> Process<'a> {
         Ok(())
     }
 
+    // TODO
     pub fn yield_process (&self) -> () {
         let ref vital = unsafe { &*self.vital };
     }
 
+    // Set's the process' stack
     pub fn set_stack_pointer (&mut self, number_of_processes: usize) -> Result<(), ProcessError> {
         if self.state != ProcessState::CREATED {
             return Err(ProcessError::ProcessAlreadyRunning);
@@ -112,11 +132,14 @@ impl<'a> Process<'a> {
         Ok(())
     }
 
+    // Sets the time quantum
     pub fn set_time_quantum (&mut self, quantum: i32) -> Result<(), ProcessError> {
         self.quantum = quantum;
         Ok(())
     }
 
+    // Sends a tick singal to the process and returns
+    // the indicator if the process should change
     pub fn tick (&mut self) -> ProcessTickResult
     {
         self.remaining = self.remaining - 1;
@@ -128,6 +151,7 @@ impl<'a> Process<'a> {
         ProcessTickResult::DontYield
     }
 
+    // Indicates if the process is ready
     pub fn is_process_ready (&self) -> bool {
         self.state != ProcessState::BLOCKED &&
             self.state != ProcessState::STOPPED
