f107057632d780a40d01fb328222cfcbe62ef1eb
Nemanja Boric <4burgos@gmail.com>
2016-06-05 10:57:52 +0200

    Compile using Cargo

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-arm-linux-gnueabi-gcc  -Wl,--build-id=none -T linker/arm.ld -nostartfiles -o kernel.elf -fpie -ffreestanding -nostdlib obj/boot.o obj/aeabi.a obj/umodsi3.o obj/udivsi3.o obj/lib.o
+arm-linux-gnueabi-gcc  -Wl,--build-id=none -T linker/arm.ld -nostartfiles -o kernel.elf -fpie -ffreestanding -nostdlib obj/boot.o obj/aeabi.a obj/umodsi3.o obj/udivsi3.o target/arm-unknown-linux-gnueabi/debug/kernel.o
 
--- rustc.sh
+++ rustc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p obj/
-~/rust/nightly/bin/rustc --out-dir=obj/ -Z no-landing-pads --crate-type staticlib --target=arm-unknown-linux-gnueabi --emit obj -C linker=arm-linux-gnueabi-gcc  -C lto -C opt-level=3 $1 -C relocation-model=dynamic-no-pic
+cargo rustc --target=arm-unknown-linux-gnueabi -- -Z no-landing-pads --crate-type staticlib  --emit obj -C linker=arm-linux-gnueabi-gcc  -C lto -C opt-level=3 -C relocation-model=dynamic-no-pic
