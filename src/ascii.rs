7119e5aaa78f46591ae75d3d6fdff3db02889358
Nemanja Boric <4burgos@gmail.com>
2016-06-05 10:32:42 +0200

    Change ascii module to use unifont

--- src/ascii.rs
+++ src/ascii.rs
@@ -5,46 +5,38 @@ use register::Register;
 use vital;
 use vital::Vital;
 use core::mem;
+use ascii_font::FONT;
 
-pub struct Character {
-    pixels: [[u8; 8]; 8]
-}
-
-static Ascii: &'static [Character; 2] = &[ 
-    Character {
-        pixels:  [
-                     [ 1, 1, 1, 1, 1, 1, 1, 1 ],
-                     [ 1, 1, 0, 0, 0, 0, 1, 1 ],
-                     [ 1, 1, 0, 0, 0, 0, 1, 1 ],
-                     [ 1, 1, 0, 0, 0, 0, 1, 1 ],
-                     [ 1, 1, 0, 0, 0, 0, 1, 1 ],
-                     [ 1, 1, 0, 0, 0, 0, 1, 1 ],
-                     [ 1, 1, 0, 0, 0, 0, 1, 1 ],
-                     [ 1, 1, 1, 1, 1, 1, 1, 1 ]
-                  ]
-    },
-    Character {
-        pixels:  [
-                     [ 0, 0, 1, 1, 1, 0, 0, 0 ],
-                     [ 0, 1, 1, 1, 1, 0, 0, 0 ],
-                     [ 0, 1, 1, 1, 1, 0, 0, 0 ],
-                     [ 0, 0, 0, 1, 1, 0, 0, 0 ],
-                     [ 0, 0, 0, 1, 1, 0, 0, 0 ],
-                     [ 0, 0, 0, 1, 1, 0, 0, 0 ],
-                     [ 0, 0, 0, 1, 1, 0, 0, 0 ],
-                     [ 0, 0, 0, 1, 1, 0, 0, 0 ]
-                  ]
-    },
-];
 
 pub fn putchar (val: char, x_offset: u32, y_offset: u32, color: u32) -> () {
-    let char_index = (val as u8 - '0' as u8) as usize;
+    let char_index = val as usize;
+
+    let bmp = FONT[char_index];
+
+    // Sample for '0'
+    // 00 00 00 00 18 24 42 42 42 42 42 42 24 18 00 00
+    // 16 rows, two digits per row, 8 bits per row.
 
     let addr = ((1024 * 1024) + ((y_offset * 640 * 4) + x_offset)) as *mut u32;
 
-    for i in 0 .. 8 {
+    // Iterate trough each row
+    for j in 0 .. 16 {
+        // Now we have 18. Convert it to value
+        let value = (bmp[((j as u32 * 2) + 0) as usize]) * 16 + (bmp[((j as u32 * 2) + 1) as usize]);
+
+        // and now for every bit in the value, show it.
+        for i in 0 .. 8 {
+            Register::new((addr as u32 + j as u32 * 640 * 4 + i * 4) as *mut u32).set(((value & (0x80 >> i)) >> (7 - i)) as u32 * color);
+        }
+    }
+    
+    /*
+    for i in 0 .. 4 {
         for j in 0 .. 8 {
+            let c = bmp[(i + j * 4) as usize];
+            // Now we have the v
             Register::new((addr as u32 + j * 640 * 4 + i * 4) as *mut u32).set(Ascii[char_index].pixels[j as usize][i as usize] as u32 * color);
         }
     }
+    */
 }
