8fb6045e02a8beea402ad7e1a96e9b5719b3b0cc
Nemanja Boric <4burgos@gmail.com>
2016-06-11 23:32:04 +0200

    Use static vital instance for SWI

--- src/swi.rs
+++ src/swi.rs
@@ -13,13 +13,19 @@ pub fn call_swi(number: u32, value_1: u32, value_2: u32) -> u32 {
 pub fn handle(vital: &mut Vital, interrupt: u32, value_1: u32, value_2: u32) -> u32 {
     match interrupt {
         1 => // running process id:
-            get_process_id(vital),
+            get_process_id(),
         _ => 0
     }
 }
 
 #[no_mangle]
-pub fn get_process_id(vital: &mut Vital) -> u32 {
+pub fn get_process_id() -> u32 {
+    let mut vital = VITAL.lock();
     let i = vital.running_process_id();
-        i
+
+    if cfg!(feature="log-swi") {
+        kprint!("Got process id for process: {}", i);
+    }
+
+    i
 }
